51537231c1898ce1d2515dd2136dc72a56554f68
Sharawn <114239056+Sharawn@users.noreply.github.com>
2024-09-22 21:03:28 +0800

    Initial commit

---
+++ controller/shopperController.js
@@ -0,0 +1,32 @@
+const main = {
+  index: (req, res) => {
+    res.render('index');
+  },
+
+  shop: (req, res) => {
+    res.render('shop');
+  },
+
+  detail: (req, res) => {
+    res.render('detail');
+  },
+
+  cart: (req, res) => {
+    res.render('cart');
+  },
+
+  checkout: (req, res) => {
+    res.render('checkout');
+  },  
+
+  contact: (req, res) => {
+    res.render('contact');
+  },
+
+  home: (req, res) => {
+    res.render('home');
+  },
+
+}
+
+module.exports = main;
---
+++ routes/router.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controller/shopperController');
+
+router.get('/', controller.index);
+router.get('/shop', controller.shop);
+router.get('/detail', controller.detail);
+router.get('/cart', controller.cart);
+router.get('/checkout', controller.checkout);
+router.get('/contact', controller.contact);
+router.get('/home', controller.home);
+
+module.exports = router;
+
